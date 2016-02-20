818ad0a984ce401ac954f1fadb238c101334f78f
Lenny Boyatzis <lennyboyatzis@gmail.com>
2016-02-20 14:44:26 +1100

    clean up

--- app/containers/ChatRoom.js
+++ app/containers/ChatRoom.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
-import SomeOtherComponent from './SomeOtherComponent'
 
 export default class ChatRoom extends Component {
   render() {
     return (
       <div>
         <p>Hello Tony johns</p>
-        <p>Hello timmy Big dog</p>
+        <p>Hello timmy</p>
       </div>
     )
   }
