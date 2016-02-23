f7442c26eb535d9396be48799f2f1b2bd00ae892
Lenny Boyatzis <lennyboyatzis@gmail.com>
2016-02-23 21:44:10 +1100

    sendMessage on keyCode === 13

--- app/components/ChatInput.js
+++ app/components/ChatInput.js
@@ -2,11 +2,19 @@ import React, { Component, PropTypes } from 'react'
 import sendMessage from '../lib/emit'
 
 export default class ChatInput extends Component {
+
+  handleInputChange (e) {
+    if (e.keyCode === 13) {
+      sendMessage({ content: this.refs.msg.value })
+      this.refs.msg.value = ''
+    }
+  }
+
   render() {
     return (
       <div className="input">
         <a className="input-link attach"></a>
-        <input type="text" autoFocus="true" placeholder="Type here to chat!" ref="msg" />
+        <input type="text" autoFocus="true" placeholder="Type here to chat!" ref="msg" onKeyDown={this.handleInputChange.bind(this)} />
         <a className="input-link smiley"></a>
         <a className="input-link send" onClick={() => sendMessage({ content: this.refs.msg.value })}></a>
       </div>
--- app/components/FriendsList.js
+++ app/components/FriendsList.js
@@ -9,7 +9,7 @@ export default class FriendsList extends Component {
         {
           PEOPLE.map((person) => {
             return (
-              <li className="person" data-chat="person1">
+              <li key={person} className="person" data-chat="person1">
                 <img src="http://s13.postimg.org/ih41k9tqr/img1.jpg" alt="" />
                 <span className="name">Thomas Bangalter</span>
                 <span className="time">2:09 PM</span>
