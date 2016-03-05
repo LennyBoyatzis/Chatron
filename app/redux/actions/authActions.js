afd231bb7e036719d998254dd995b8aa999829f6
Lenny Boyatzis <lennyboyatzis@gmail.com>
2016-03-05 14:37:52 +1100

    Added log out actions

--- app/redux/actions/authActions.js
+++ app/redux/actions/authActions.js
@@ -1,5 +1,5 @@
 import { push } from 'react-router-redux'
-import { SIGN_UP_REQUEST, SIGN_UP_SUCCESS, SIGN_UP_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE } from '../../constants/ActionTypes'
+import { SIGN_UP_REQUEST, SIGN_UP_SUCCESS, SIGN_UP_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT_REQUEST, LOGOUT_SUCCESS } from '../../constants/ActionTypes'
 import { BASE_URL } from '../../constants/App'
 
 
@@ -40,7 +40,36 @@ const signUpFailure = (msg) => {
   return { type: SIGN_UP_FAILURE, msg }
 }
 
+/**
+* Action creator which posts the users login creds and dispatches either
+* loginSuccess or loginError
+* @param { creds } user's credentials incl. username && password
+* @returns { dispatch }
+*/
 
+export function loginUser(creds) {
+  let config = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: `username=${creds.username}&password=${creds.password}`
+  }
+
+  return dispatch => {
+    dispatch(requestLogin(creds))
+
+    return fetch(`${BASE_URL}/sessions/create`, config)
+      .then(response => response.json().then(user => ({ user, response })))
+      .then(({ user, response }) => {
+        if (!reponse.ok) {
+          dispatch(loginError(user.message))
+          return Promise.reject(user)
+        } else {
+          localStorage.setItem('id_token', user.id_token)
+          dispatch(loginSucess(user))
+        }
+      }).catch(err => console.log("Error: ", err))
+  }
+}
 
 const loginRequest = (creds) => {
   return {
@@ -66,5 +95,34 @@ const loginError = (message) => {
     isFetching: false,
     isAuthenticated: false,
     message
+  }
+}
+
+/**
+* Action creator which is responsible for logging the user out. It removes the local storage token and dispatches LOGOUT_SUCCESS action
+* @returns { dispatch }
+*/
+
+export function logoutUser() {
+  return dispatch => {
+    dispatch(logoutRequest())
+    localStorage.removeItem('id_token')
+    dispatch(logoutSucess())
+  }
+}
+
+const logoutRequest = () => {
+  return {
+    type: LOGOUT_REQUEST,
+    isFetching: true,
+    isAuthenticated: true
+  }
+}
+
+const logoutSucess = () => {
+  return {
+    type: LOGOUT_SUCCESS,
+    isFetching: false,
+    isAuthenticated: false
   }
 }
