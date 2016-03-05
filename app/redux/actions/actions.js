e2fa8483bb1f83f64bd37beea30c32a9b821145d
Lenny Boyatzis <lennyboyatzis@gmail.com>
2016-03-05 19:52:35 +1100

    fetching all available users

--- app/redux/actions/actions.js
+++ app/redux/actions/actions.js
@@ -1,6 +1,7 @@
 import socket from '../../lib/socket'
 import { types } from '../../constants/ActionTypes'
-import { browserHistory } from 'react-router';
+import { browserHistory } from 'react-router'
+import { BASE_URL } from '../../constants/App'
 
 export function receiveMessage(message) {
   return { type: types.RECEIVE_MESSAGE, message }
@@ -8,4 +9,23 @@ export function receiveMessage(message) {
 
 export function addUser(user) {
   return { type: types.ADD_USER, user }
+}
+
+export function fetchAvailableUsers() {
+
+  let config = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+  }
+
+  return dispatch => {
+    dispatch(fetchAvailableUsersRequest())
+    return fetch(`${BASE_URL}/api/users/get`, config)
+      .then(response => res.json())
+      .then(response => {
+        if (response.err) return dispatch(fetchAvailableUsersFailure())
+        dispatch(fetchAvailableUsersSuccess(users))
+      }).catch(err => { throw err })
+  }
+
 }
