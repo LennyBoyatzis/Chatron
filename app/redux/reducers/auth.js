8749ed61bd709c47e532c0b4700a3d29399dca6b
Lenny Boyatzis <lennyboyatzis@gmail.com>
2016-03-05 14:47:08 +1100

    Added login & logout cases to auth reducer

--- app/redux/reducers/auth.js
+++ app/redux/reducers/auth.js
@@ -2,7 +2,10 @@ import _ from 'lodash'
 import deepFreeze from 'deep-freeze'
 import { SIGN_UP_FAILURE, SIGN_UP_REQUEST, SIGN_UP_SUCCESS } from '../../constants/ActionTypes'
 
-const initialState = {}
+const initialState = {
+  isFetching: false,
+  isAuthenticated: !!localStorage.getItem('id_token')
+}
 
 deepFreeze(initialState)
 
@@ -24,6 +27,33 @@ export function auth(state = initialState, action) {
         },
         signUpError: true,
         signUpErrorMsg: msg.err
+      })
+
+    case LOGIN_REQUEST:
+      return Object.assign({}, state, {
+        isFetching: true,
+        isAuthenticated: false,
+        user: action.creds
+      })
+
+    case LOGIN_SUCCESS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        isAuthenticated: true,
+        errorMessage: ''
+      })
+
+    case LOGIN_FAILURE:
+      return Object.assign({}, state, {
+        isFetching: false,
+        isAuthenticated: false,
+        errorMessage: action.message
+      })
+
+    case LOGOUT_SUCCESS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        isAuthenticated: false
       })
 
     default:
