e73c41b77da64972e98c7145f22c29ba8ab18250
Lenny Boyatzis <lennyboyatzis@gmail.com>
2016-03-06 17:47:28 +1100

    finally it works...

--- app/redux/reducers/messages.js
+++ app/redux/reducers/messages.js
@@ -5,18 +5,22 @@ import { RECEIVE_MESSAGE } from '../../constants/ActionTypes'
 // This part of the state tree needs to reflect all of the logged in users correspondence with other users
 // These messages should only be created once having clicked on a user
 
-const initialState = {}
+// userId = 'IDIvB3'
+//
+// {
+//   IDq7jz: [
+//     { from: 'IDIvB3', message: 'How are you going'}
+//     { from: 'IDq7jz', message: 'Pretty good and you?'}
+//     { from: 'IDIvB3', message: 'Yeah not bad thanks'}
+//   ]
+// }
 
-deepFreeze(initialState)
-
-export function messages(state = initialState, action) {
+export function messages(state = {}, action) {
   switch (action.type) {
     case RECEIVE_MESSAGE:
       const { content, toUser, fromUser } = action.msg
-      console.log('content----->', content);
-      console.log('toUser----->', toUser);
-      console.log('fromUser----->', fromUser);
-      return Object.assign({}, state, { [toUser]: [].concat({ from: fromUser, content }) })
+      const userArray = state[toUser] ? state[toUser] : []
+      return Object.assign({}, state, { [fromUser]: userArray.concat({ from: fromUser, content }) })
     default:
       return state
   }
