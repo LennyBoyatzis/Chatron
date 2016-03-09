7e76a0c6eb5afcf00cda9513145696524d5014a7
Lenny Boyatzis <lennyboyatzis@gmail.com>
2016-03-10 09:23:36 +1100

    Added logout link

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+import { logoutUser } from '../redux/actions/authActions'
 
 export default class NavBar extends Component {
+
+  handleSubmit (e) {
+    const { dispatch } = this.props
+    dispatch(logoutUser(userId));
+  }
+
   render () {
     return (
       <div className="nav-bar">
         <span>To:<strong> Lenny</strong></span>
-        <span className="right">Logout</span>
+        <span onClick={ this.handleSubmit.bind(this) } className="right">Logout</span>
       </div>
     )
   }
