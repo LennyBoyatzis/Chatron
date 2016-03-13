cfec7853a4d12968cbf0293fab454392a9505347
Lenny Boyatzis <lennyboyatzis@gmail.com>
2016-03-13 12:15:50 +1100

    Chatroom Window component complete

--- app/components/ChatroomWindow.js
+++ app/components/ChatroomWindow.js
@@ -5,7 +5,10 @@ export default class ChatroomWindow extends Component {
     return (
       <div className="form-group chatroom-window">
         <div className="form-group chatroom-content">
-          
+          <div className="bubble you">Hi, How are you?</div>
+          <div className="bubble me">I am good thanks and you?</div>
+          <div className="bubble you">Yeah not too bad, glad it's the weekend. What you doing this weekend?</div>
+          <div className="bubble me">Weather is great so I'll probably head to the beach</div>
         </div>
         <div className="input">
           <input type="text" autoFocus="true" placeholder="Type here to chat!" ref="msg"  className="form-control input" />
