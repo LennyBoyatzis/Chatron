05c55b6870f09ab4cf06cc352881c9cad6ea12db
Lenny Boyatzis <lennyboyatzis@gmail.com>
2016-03-26 10:52:06 +1100

    removed history from react router

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 import "./less/site.less";
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, hashHistory, Route, IndexRoute } from 'react-router'
+import { Router, Route, IndexRoute } from 'react-router'
 import { Provider } from 'react-redux'
 import App from './containers/App'
 import Login from './containers/Login'
@@ -14,7 +14,7 @@ const store = configureStore()
 
 ReactDOM.render((
   <Provider store={ store }>
-    <Router history={ hashHistory } >
+    <Router>
       <Route path='/' component={ App } >
         <IndexRoute component={ Login } />
         <Route path='/signup' component={ Signup } />
